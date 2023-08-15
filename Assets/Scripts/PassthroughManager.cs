cc13540c4fbcd7083154ae8b405a19a5c298f892
Debbie Yuen <deborahyuen2016@berkeley.edu>
2023-08-15 15:51:26 -0700

    add OculusXR for passthrough functionality

---
+++ Assets/Scripts/PassthroughManager.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PassthroughManager : MonoBehaviour
+{
+	public OVRPassthroughLayer passthrough;
+
+    public OVRInput.Button button;
+
+    public OVRInput.Controller controller;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (OVRInput.GetDown(button, controller))
+        {
+            passthrough.hidden = !passthrough.hidden;
+        }
+    }
+}
